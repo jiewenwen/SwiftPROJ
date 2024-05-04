61f68abc06d2e70711980f457d484d071a6cdc85
jiewenwen <jiewen.wen@qq.com>
2024-05-04 15:33:33 +0800

    add searchableLists

---
+++ SearchableLists/SearchableLists/SearchableListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableListsApp.swift
+//  SearchableLists
+//
+//  Created by Jiewen Wen on 2024/5/4.
+//
+
+import SwiftUI
+
+@main
+struct SearchableListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
