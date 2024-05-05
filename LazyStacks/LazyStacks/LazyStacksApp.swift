3fe37ef078b6aafd99474a4df0bfbb2ceac2bf94
jiewenwen <jiewen.wen@qq.com>
2024-05-05 15:21:19 +0800

    add laztStasks

---
+++ LazyStacks/LazyStacks/LazyStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyStacksApp.swift
+//  LazyStacks
+//
+//  Created by Jiewen Wen on 2024/5/4.
+//
+
+import SwiftUI
+
+@main
+struct LazyStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
