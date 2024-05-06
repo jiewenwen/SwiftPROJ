163c26dac7ababdafb4852002cf99c72bfba7bd7
jiewenwen <jiewen.wen@qq.com>
2024-05-06 15:55:59 +0800

    add scrollViewReaders

---
+++ ScrollViewReaders/ScrollViewReaders/ScrollViewReadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReadersApp.swift
+//  ScrollViewReaders
+//
+//  Created by Jiewen Wen on 2024/5/5.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
