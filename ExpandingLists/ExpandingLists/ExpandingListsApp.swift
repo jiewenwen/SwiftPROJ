91df6baaa459f4b17e23f5cf5ee971701c53559e
jiewenwen <jiewen.wen@qq.com>
2024-05-21 15:11:15 +0800

    add ExpandingLists

---
+++ ExpandingLists/ExpandingLists/ExpandingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingListsApp.swift
+//  ExpandingLists
+//
+//  Created by Jiewen Wen on 2024/5/6.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
