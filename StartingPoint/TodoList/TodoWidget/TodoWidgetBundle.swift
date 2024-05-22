5b0a279df4d650659df2cb27cde271f68c1f722a
jiewenwen <jiewen.wen@qq.com>
2024-05-22 18:14:26 +0800

    StartingPoint

---
+++ StartingPoint/TodoList/TodoWidget/TodoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoWidgetBundle.swift
+//  TodoWidget
+//
+//  Created by Jiewen Wen on 2024/5/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TodoWidget()
+    }
+}
