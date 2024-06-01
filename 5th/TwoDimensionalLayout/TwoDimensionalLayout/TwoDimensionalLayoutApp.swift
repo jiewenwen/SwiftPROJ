5ef25d4fde5727f6ecd6aab7040da20d17a472c7
jiewenwen <jiewen.wen@qq.com>
2024-06-01 16:15:54 +0800

    add Grid...

---
+++ 5th/TwoDimensionalLayout/TwoDimensionalLayout/TwoDimensionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoDimensionalLayoutApp.swift
+//  TwoDimensionalLayout
+//
+//  Created by Jiewen Wen on 2024/6/1.
+//
+
+import SwiftUI
+
+@main
+struct TwoDimensionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
