599678766ec88e2c73805239eec77ad627c9b4a3
jiewenwen <jiewen.wen@qq.com>
2024-06-18 21:11:35 +0800

    add drawing

---
+++ 8th/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jiewen Wen on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
