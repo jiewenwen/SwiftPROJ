a4b8d08e0955bc139b7a92542ba5e9ae99aef75f
jiewenwen <jiewen.wen@qq.com>
2024-06-18 17:26:17 +0800

    add heart

---
+++ 8th/Heart/Heart/HeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartApp.swift
+//  Heart
+//
+//  Created by Jiewen Wen on 2024/6/18.
+//
+
+import SwiftUI
+
+@main
+struct HeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
