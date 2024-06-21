3a15a259db983462b9a4220f0376590f6bd13dfa
jiewenwen <jiewen.wen@qq.com>
2024-06-21 19:16:52 +0800

    add staticTodoLists

---
+++ 10th/staticTodoList/staticTodoList/staticTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  staticTodoListApp.swift
+//  staticTodoList
+//
+//  Created by Jiewen Wen on 2024/6/20.
+//
+
+import SwiftUI
+
+@main
+struct staticTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
