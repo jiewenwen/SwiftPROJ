fcf7702cf49ea460c0b25e3d14fa483743134417
jiewenwen <jiewen.wen@qq.com>
2024-06-27 21:37:33 +0800

    add commine...

---
+++ 11th/CombineCoreLocationManager/CombineCoreLocationManager/CombineCoreLocationManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCoreLocationManagerApp.swift
+//  CombineCoreLocationManager
+//
+//  Created by Jiewen Wen on 2024/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CombineCoreLocationManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
