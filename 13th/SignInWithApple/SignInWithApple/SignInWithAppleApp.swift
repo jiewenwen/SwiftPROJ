af566176d1143bfb59a9fb55525d1746c374fc16
jiewenwen <jiewen.wen@qq.com>
2024-07-05 18:56:53 +0800

    add signinwithapple

---
+++ 13th/SignInWithApple/SignInWithApple/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  SignInWithApple
+//
+//  Created by Jiewen Wen on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
